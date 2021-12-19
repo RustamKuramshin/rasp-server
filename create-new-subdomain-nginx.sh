bbc821f75ef3dea176df36b0a5fd5401d58872fb
rustam <kuramshin.py@yandex.ru>
2021-12-19 15:20:28 +0300

    fix script

--- create-new-subdomain-nginx.sh
+++ create-new-subdomain-nginx.sh
@@ -28,7 +28,7 @@ server {
         server_name $1.${DOMAIN} www.$1.${DOMAIN};
 
         location / {
-                try_files $uri $uri/ =404;
+          try_files \$uri \$uri/ =404;
         }
 }
 EOF
@@ -39,8 +39,8 @@ sudo systemctl restart nginx && \
 sudo certbot --nginx -d $1.${DOMAIN} -d www.$1.${DOMAIN}
 
 NGINX_TRY_FILES="try_files \$uri \$uri\/ =404;"
-NGINX_PROXY_PASS="proxy_pass http:\/\/localhost:$2;"
-sed -i "s/${NGINX_TRY_FILES}/${NGINX_PROXY_PASS}/g" /etc/nginx/sites-available/$1.${DOMAIN}
+NGINX_COMMENT_TRY_FILES_AND_PROXY_PASS="#try_files \$uri \$uri\/ =404;\n          proxy_pass http:\/\/localhost:$2;"
+sed -i "s/${NGINX_TRY_FILES}/${NGINX_COMMENT_TRY_FILES_AND_PROXY_PASS}/g" ./$1.${DOMAIN}
 
 sudo nginx -t && \
 sudo systemctl restart nginx
